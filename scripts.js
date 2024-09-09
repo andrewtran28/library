4a9f252fe7748b44c7dc6f96e56b24ee6856f8f1
andrewtran28 <tran.andrew@outlook.com>
2024-09-09 19:18:44 -0400

    Implement form toggling

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 const myLibrary = [];
+const form = document.querySelector("form");
 const btn_addBook = document.querySelector("#add-book");
-const addBookForm = document.querySelector(".form-toggle");
+const btn_submit = document.querySelector("#submit")
+const toggleForm = document.querySelector(".form-toggle");
+const btn_remove = document.querySelector(".remove");
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -10,28 +13,63 @@ function Book(title, author, pages, read) {
 
     this.info = function() {
         read ? read = "has been read" : read = "has not been read yet";
-        return (title + "by " + author + ", " + pages + " pages, " + read + ".");
+        return (title + " by " + author + " (" + pages + " pages) " + read + ".");
     }
 
     console.log(this.info());
 }
 
-function addBookToLibrary() {
-    // const title_input = document.querySelector("#password");
-    // const author_input = document.querySelector("#confirm-password");
-    // const pages_input = document.querySelector(".message");
-    // const read_input = document.querySelector("form");
+function addBookCard(title, author, pages, read) {
+    const newBook = document.querySelector(".library-cont");
+    const card = document.createElement("div");
+    const bookTitle = document.createElement("div");
+    const bookAuthor = document.createElement("div");
+    const bookPages = document.createElement("div");
+    const bookStatus = document.createElement("div");
+    const bookRemove = document.createElement("button");
 
-    // form.addEventListener("submit", e => {
-    //     const pwd = document.querySelector('#password').value;
-    //     const confirm_pwd = document.querySelector('#confirm-password').value;
-    //     const pwd_msg = document.querySelector('.pwd-msg'); 
-    // });
+    card.classList.add("book");
+    bookTitle.classList.add("title");
+    bookAuthor.classList.add("author");
+    bookRemove.classList.add("remove");
+    // bookPages.classList.add("pages");
+
+    newBook.appendChild(card);
+    card.appendChild(bookRemove);
+    card.appendChild(bookTitle);
+    card.appendChild(bookAuthor);
+    card.appendChild(bookPages);
+    card.appendChild(bookStatus);
+
+    bookTitle.textContent = title;
+    bookAuthor.textContent = author;
+    bookPages.textContent ="Page Count: " + pages;
+    bookStatus.textContent = "Read Status: " + read;
+    bookRemove.textContent = "x";
+
+    btn_remove.addEventListener("click", () => {
+        newBook.removeChild(card);
+        console.log("Test");
+    });
+
+}
+
+function addBookToLibrary(event) {
+    const title_input = document.querySelector("#book-title").value;
+    const author_input = document.querySelector("#book-author").value;
+    const pages_input = document.querySelector("#book-pages").value;
+    const read_input = document.querySelector("#book-status").value;
+    event.preventDefault();
+
+    let newBook = new Book(title_input, author_input, pages_input, read_input);
+    addBookCard(title_input, author_input, pages_input, read_input);
+    toggleForm.classList.toggle("form-toggle");
 }
 
 btn_addBook.addEventListener("click", () => {
-    addBookForm.classList.toggle("form-toggle");
+    toggleForm.classList.toggle("form-toggle");
 });
 
+form.addEventListener("submit", addBookToLibrary);
+
 const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, 0);
-hobbit.info();
