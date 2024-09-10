1011f125f16a6f35893f9097e5725d76a14ce1e2
andrewtran28 <tran.andrew@outlook.com>
2024-09-10 16:44:06 -0400

    Functioning library

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 const myLibrary = [];
-
+var totalBooks = myLibrary.length;
+const total = document.querySelector(".total")
+const dialog = document.querySelector("dialog");
+const btn_close = document.querySelector("dialog button")
 const form = document.querySelector("form");
 const btn_addBook = document.querySelector("#add-book");
-const btn_submit = document.querySelector("#submit")
-const toggleForm = document.querySelector(".form-toggle");
 const btn_remove = document.querySelector(".remove");
+// const btn_submit = document.querySelector("#submit")
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -20,6 +22,13 @@ function Book(title, author, pages, read) {
     console.log(this.info());
 }
 
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+
+    myLibrary.push(newBook);
+    displayLibrary();
+}
+
 function displayLibrary() {
     let library = document.querySelector(".library-cont");
 
@@ -31,7 +40,7 @@ function displayLibrary() {
         card.className = "book";
       
         card.innerHTML = `
-            <button class="remove">x</button>
+            <button class="remove" onClick="removeBook(${i})">Close</button>
             <div class="title">${book.title}</div>
             <div class="author">${book.author}</div>
             <div class="page">Page Count: ${book.pages}</div>
@@ -40,66 +49,50 @@ function displayLibrary() {
                 <input type="checkbox" id="read" name="read" ${book.read ? "checked" : "unchecked"}>
             </div>
         `;
-
         library.appendChild(card);
+        total.textContent = "Total Books: " + myLibrary.length; 
+        dialog.close();
     }
-    
 }
 
-function addBookCard(title, author, pages, read) {
-    const newBook = document.querySelector(".library-cont");
-    const card = document.createElement("div");
-    const bookTitle = document.createElement("div");
-    const bookAuthor = document.createElement("div");
-    const bookPages = document.createElement("div");
-    const bookStatus = document.createElement("div");
-    const bookRemove = document.createElement("button");
-
-    card.classList.add("book");
-    bookTitle.classList.add("title");
-    bookAuthor.classList.add("author");
-    bookRemove.classList.add("remove");
-    // bookPages.classList.add("pages");
-
-    newBook.appendChild(card);
-    card.appendChild(bookRemove);
-    card.appendChild(bookTitle);
-    card.appendChild(bookAuthor);
-    card.appendChild(bookPages);
-    card.appendChild(bookStatus);
-
-    bookTitle.textContent = title;
-    bookAuthor.textContent = author;
-    bookPages.textContent ="Page Count: " + pages;
-    bookStatus.textContent = "Read Status: " + read;
-    bookRemove.textContent = "x";
-
-    btn_remove.addEventListener("click", () => {
-        document.removeChild(card);
-        console.log("Test");
-    });
-
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+    total.textContent = "Total Books: " + myLibrary.length; 
+    displayLibrary();
 }
 
-function addBookToLibrary(event) {
-    event.preventDefault();
+btn_addBook.addEventListener("click", () => {
+    dialog.showModal();
+    form.reset();
+    // toggleForm.classList.toggle("form-toggle");
+});
 
+btn_close.addEventListener("click", () => {
+    form.reset();
+    dialog.close();
+});
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
     const title_input = document.querySelector("#book-title").value;
     const author_input = document.querySelector("#book-author").value;
     const pages_input = document.querySelector("#book-pages").value;
     const read_input = document.querySelector("#book-status").checked;
+    var pages = pages_input;
 
-    let newBook = new Book(title_input, author_input, pages_input, read_input);
-
-    myLibrary.push(newBook);
-    displayLibrary();
-
-    toggleForm.classList.toggle("form-toggle");
-}
-
-btn_addBook.addEventListener("click", () => {
-    form.reset();
-    toggleForm.classList.toggle("form-toggle");
+    if (title_input != "") {
+        if (pages_input < 1 || pages_input == "") {
+            pages = 0
+        };
+        addBookToLibrary(title_input, author_input, pages, read_input);
+    }
 });
 
-form.addEventListener("submit", addBookToLibrary);
+addBookToLibrary("Joe", "Mama", 142, 1);
+addBookToLibrary("The Hobbit", "Joe Mama", 1422, 0);
+addBookToLibrary("Joe", "Mama", 142, 1);
+addBookToLibrary("The Hobbit", "Joe Mama", 1422, 0);
+addBookToLibrary("Joe", "Mama", 142, 1);
+addBookToLibrary("The Hobbit", "Joe Mama", 1422, 0);
+
+console.log(totalBooks);
